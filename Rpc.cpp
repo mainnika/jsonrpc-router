540e2f208be5887d39ca846aaf7942ac5b875350
mainnika <nikita@tokarch.uk>
2016-09-21 15:31:07 +0500

    do not use abstractserver

--- Rpc.cpp
+++ Rpc.cpp
@@ -5,37 +5,40 @@
 
 #include <functional>
 
-#include <jsonrpccpp/common/procedure.h>
+#include <jsonrpccpp/server/requesthandlerfactory.h>
 
 Rpc::Rpc(Server *server) :
-jsonrpc::AbstractServer<Rpc>(*server, jsonrpc::JSONRPC_SERVER_V2),
-methods({
-    {"AuthRequest", new Authorizer()},
-}
-) {
+server(server), handler(jsonrpc::RequestHandlerFactory::createProtocolHandler(jsonrpc::JSONRPC_SERVER_V2, *this)) {
 
-    //    TODO: UGLY
-    this->bindAndAddMethod(jsonrpc::Procedure("AuthRequest", jsonrpc::PARAMS_BY_NAME, jsonrpc::JSON_STRING, NULL), &Rpc::dummy);
+    this->server->SetHandler(this->handler);
+
+    this->add_method(jsonrpc::Procedure("AuthRequest", jsonrpc::PARAMS_BY_NAME, jsonrpc::JSON_STRING), new Authorizer());
 }
 
 Rpc::~Rpc() {
 }
 
-void Rpc::HandleMethodCall(jsonrpc::Procedure& proc, const Json::Value& input, Json::Value& output) {
-    auto name = proc.GetProcedureName();
+void Rpc::add_method(const jsonrpc::Procedure &proc, AbstractService* handler) {
 
+    auto name = proc.GetProcedureName();
+    Rpc::methods_t::accessor accessor;
+
+    if (this->methods.insert(accessor, name)) {
+        accessor->second = handler;
+        this->handler->AddProcedure(proc);
+    }
+}
+
+void Rpc::HandleMethodCall(jsonrpc::Procedure& proc, const Json::Value& input, Json::Value& output) {
+
+    auto name = proc.GetProcedureName();
     Rpc::methods_t::accessor accessor;
 
     if (this->methods.find(accessor, name)) {
         accessor->second->execute(input, output);
-        return;
     }
 }
 
 void Rpc::HandleNotificationCall(jsonrpc::Procedure& proc, const Json::Value& input) {
-
-}
-
-void Rpc::dummy(const Json::Value& input, Json::Value& output) {
 
 }
--- Rpc.h
+++ Rpc.h
@@ -6,27 +6,32 @@
 
 #include <string>
 
-#include <jsonrpccpp/server.h>
+#include <jsonrpccpp/common/procedure.h>
+#include <jsonrpccpp/server/iprocedureinvokationhandler.h>
+#include <jsonrpccpp/server/iclientconnectionhandler.h>
 
 #include <tbb/concurrent_hash_map.h>
 
 class Server;
 class AbstractService;
 
-class Rpc : public jsonrpc::AbstractServer<Rpc>, private NoCopy {
+class Rpc : public jsonrpc::IProcedureInvokationHandler, private NoCopy {
 public:
 
     Rpc(Server *server);
     virtual ~Rpc();
 
+    void add_method(const jsonrpc::Procedure &proc, AbstractService *handler);
+
 private:
     typedef tbb::concurrent_hash_map<std::string, AbstractService*> methods_t;
+    typedef jsonrpc::IProtocolHandler handler_t;
 
     Mqp mqp;
+    Server *server;
     methods_t methods;
+    handler_t *handler;
 
     void HandleMethodCall(jsonrpc::Procedure& proc, const Json::Value& input, Json::Value& output) override;
     void HandleNotificationCall(jsonrpc::Procedure& proc, const Json::Value& input) override;
-
-    void dummy(const Json::Value& input, Json::Value& output);
 };
